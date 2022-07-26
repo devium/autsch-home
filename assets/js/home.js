be133c0d0bf58e377694abf5e161498beedebf2b
Georg Guba <georg.guba@gmail.com>
2022-07-27 01:39:41 +0200

    charlotte

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,6 @@
+var charlotte = false;
+var charlotteAnimation;
+
 document.addEventListener('DOMContentLoaded', function() {
   if (location.hash !== null && location.hash !== '') {
     const id = '#accordion-' + location.hash.substr(1)
@@ -5,7 +8,48 @@ document.addEventListener('DOMContentLoaded', function() {
     $(id).collapse('show');
     window.scrollTo(0, 0);
   }
+
+  resetCursors();
+  $('#charlotte').click(toggleCharlotte);
 });
+
+function resetCursors() {
+  $('html').css({
+    '--cursor-default': 'default',
+    '--cursor-pointer': 'pointer',
+    '--cursor-text': 'text'
+  });
+}
+
+function toggleCharlotte() {
+  if (charlotte) {
+    clearInterval(charlotteAnimation);
+    resetCursors();
+  } else {
+    {{ $charlotteDefault := resources.Get "/img/charlotte.png" }}
+    {{ $charlottePointing := resources.Get "/img/charlotte_pointing.png" }}
+    $('html').css({
+      '--cursor-default': 'url({{ $charlottePointing.RelPermalink }}) 21 5, auto',
+      '--cursor-pointer': 'url({{ $charlotteDefault.RelPermalink }}) 21 5, auto',
+      '--cursor-text': 'url({{ $charlottePointing.RelPermalink }}) 21 5, auto'
+    });
+
+    const setDefaultCharlotte = function() {
+      if (charlotte) {
+        $('html').css({
+          '--cursor-pointer': 'url({{ $charlotteDefault.RelPermalink }}) 21 5, auto',
+        })
+      }
+    };
+    charlotteAnimation = setInterval(function() {
+      $('html').css({
+        '--cursor-pointer': 'url({{ $charlottePointing.RelPermalink }}) 21 5, auto',
+      });
+      setTimeout(setDefaultCharlotte, 100);
+    }, 200);
+  }
+  charlotte = !charlotte;
+}
 
 $('.accordion-button.anchor').click(function(e) {
   const button = $(e.delegateTarget);
